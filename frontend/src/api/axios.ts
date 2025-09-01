6fb279f5526eaba8414521dc6f47bb4f6af8d415
ashishkumar2876 <157505007+ashishkumar2876@users.noreply.github.com>
2025-09-01 22:08:19 +0530

    correct a minor mistake in api in frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-    import { useAuthStore } from "../store/auth";
+    import { useAuthStore } from "../store/auth/api";
 
     const api = axios.create({
       baseURL: "https://note-app-backend-pgqm.onrender.com",
